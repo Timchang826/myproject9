fb0a8104732f96778afd6acc17df705bcdf78512
timchang <timchang0001@gmail.com>
2022-06-10 16:17:14 +0800

    Hello World with Kotlin

---
+++ src/main/java/PersonKotlin.kt
@@ -0,0 +1,14 @@
+package com.tim
+
+class PersonKotlin {
+    fun greeting(){
+        println("Hello Kotlin")
+    }
+}
+
+
+//Top Level
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+}
