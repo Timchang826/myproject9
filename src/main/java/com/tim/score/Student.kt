78f4eb2c79423169de2549fa51e34fabc4bbd7e9
timchang <timchang0001@gmail.com>
2022-06-17 15:06:03 +0800

    Scoring with Kotlin average

--- src/main/java/com/tim/score/Student.kt
+++ src/main/java/com/tim/score/Student.kt
@@ -4,8 +4,19 @@ class Student(var name:String,var eng:Int,var math:Int){
     init {
         println("Test")
     }
+    fun pprint(){
+        println("$name\t$eng\t$math${getAverage()}")
+    }
+    fun print(){
+        println(name + "\t" + eng + "\t" + math)
+    }
+    fun getAverage():Int{
+        return (eng+math)/2
+    }
 }
 fun main(){
     val stu = Student("Jack",55,60)
     println(stu.eng)
+    stu.print()
+    stu.pprint()
 }
